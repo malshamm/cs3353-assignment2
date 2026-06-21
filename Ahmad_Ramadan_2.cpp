80248a22006316af1b82315e125ed4ae7f21a75d
malshamm <alshmmrii.12365.3@gmail.com>
2026-06-21 23:23:14 +0000

    Completed Lab

--- Ahmad_Ramadan_2.cpp
+++ Ahmad_Ramadan_2.cpp
@@ -1,176 +1,323 @@
 /* Assignment #2, Ahmad Ramadan */
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
-struct NodeSLL {
+struct SLLNode {
     string ip;
-    NodeSLL* next;
+    SLLNode* next;
+    
+    SLLNode(string value) {
+        ip = value;
+        next = nullptr;
+    }
 };
 
-struct NodeDLL {
-    string ip;
-    NodeDLL* prev;
-    NodeDLL* next;
+class SLL {
+private:
+    SLLNode* head;
+    SLLNode* tail;
+
+public:
+    SLL() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void insertHead(string ip) {
+        SLLNode* newNode = new SLLNode(ip);
+        newNode->next = head;
+        head = newNode;
+        if (tail == nullptr) {
+            tail = head;
+        }
+    }
+
+    void insertTail(string ip) {
+        SLLNode* newNode = new SLLNode(ip);
+        if (tail == nullptr) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void deleteHead() {
+        if (head == nullptr) {
+            cout << "Error: The list is empty!" << endl;
+            return;
+        }
+        SLLNode* temp = head;
+        head = head->next;
+        delete temp;
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+    }
+
+    void deleteTail() {
+        if (head == nullptr) {
+            cout << "Error: The list is empty!" << endl;
+            return;
+        }
+        if (head == tail) {
+            delete head;
+            head = tail = nullptr;
+            return;
+        }
+        SLLNode* curr = head;
+        while (curr->next != tail) {
+            curr = curr->next;
+        }
+        delete tail;
+        tail = curr;
+        tail->next = nullptr;
+    }
+
+    void searchAndDelete(string targetIP) {
+        if (head == nullptr) {
+            cout << "Error: The list is empty!" << endl;
+            return;
+        }
+        
+        // If the target is the head
+        if (head->ip == targetIP) {
+            deleteHead();
+            return;
+        }
+
+        SLLNode* curr = head;
+        while (curr->next != nullptr && curr->next->ip != targetIP) {
+            curr = curr->next;
+        }
+
+        if (curr->next != nullptr) {
+            SLLNode* temp = curr->next;
+            curr->next = temp->next;
+            if (temp == tail) {
+                tail = curr;
+            }
+            delete temp;
+        } else {
+            cout << "There is no such node in the list!" << endl;
+        }
+    }
+
+    void printList() {
+        SLLNode* curr = head;
+        while (curr != nullptr) {
+            cout << curr->ip << " ";
+            curr = curr->next;
+        }
+        cout << endl;
+    }
 };
 
-NodeSLL* sllHead = nullptr;
 
-NodeDLL* dllHead = nullptr;
-NodeDLL* dllTail = nullptr;
-
-string extractIP(string line) {
-    return line.substr(0, line.find(' '));
-}
-
-void SLL_InsertHead(string ip) {
-    NodeSLL* newNode = new NodeSLL{ip, sllHead};
-    sllHead = newNode;
-}
-
-void SLL_InsertTail(string ip) {
-    NodeSLL* newNode = new NodeSLL{ip, nullptr};
-    if (!sllHead) { sllHead = newNode; return; }
-    NodeSLL* temp = sllHead;
-    while (temp->next) temp = temp->next;
-    temp->next = newNode;
-}
-
-void SLL_DeleteHead() {
-    if (!sllHead) { cout << "Empty list!" << endl; return; }
-    NodeSLL* temp = sllHead;
-    sllHead = sllHead->next;
-    delete temp;
-}
-
-void SLL_DeleteTail() {
-    if (!sllHead) { cout << "Empty list!" << endl; return; }
-    if (!sllHead->next) { delete sllHead; sllHead = nullptr; return; }
-    NodeSLL* temp = sllHead;
-    while (temp->next->next) temp = temp->next;
-    delete temp->next;
-    temp->next = nullptr;
-}
-
-void SLL_SearchDelete(string ip) {
-    if (!sllHead) { cout << "Empty list!" << endl; return; }
-    if (sllHead->ip == ip) { SLL_DeleteHead(); return; }
-    NodeSLL* temp = sllHead;
-    while (temp->next && temp->next->ip != ip) temp = temp->next;
-    if (temp->next) {
-        NodeSLL* toDelete = temp->next;
-        temp->next = temp->next->next;
-        delete toDelete;
-    } else {
-        cout << "There is no such node in the list!" << endl;
+struct DLLNode {
+    string ip;
+    DLLNode* prev;
+    DLLNode* next;
+    
+    DLLNode(string value) {
+        ip = value;
+        prev = nullptr;
+        next = nullptr;
     }
-}
+};
 
-void SLL_Print() {
-    NodeSLL* temp = sllHead;
-    while (temp) {
-        cout << temp->ip << " ";
-        temp = temp->next;
+class DLL {
+private:
+    DLLNode* head;
+    DLLNode* tail;
+
+public:
+    DLL() {
+        head = nullptr;
+        tail = nullptr;
     }
-    cout << endl;
-}
 
-void DLL_InsertHead(string ip) {
-    NodeDLL* newNode = new NodeDLL{ip, nullptr, dllHead};
-    if (dllHead) dllHead->prev = newNode;
-    else dllTail = newNode;
-    dllHead = newNode;
-}
+    void insertHead(string ip) {
+        DLLNode* newNode = new DLLNode(ip);
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
 
-void DLL_InsertTail(string ip) {
-    NodeDLL* newNode = new NodeDLL{ip, dllTail, nullptr};
-    if (dllTail) dllTail->next = newNode;
-    else dllHead = newNode;
-    dllTail = newNode;
-}
+    void insertTail(string ip) {
+        DLLNode* newNode = new DLLNode(ip);
+        if (tail == nullptr) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
 
-void DLL_DeleteHead() {
-    if (!dllHead) { cout << "Empty list!" << endl; return; }
-    NodeDLL* temp = dllHead;
-    dllHead = dllHead->next;
-    if (dllHead) dllHead->prev = nullptr;
-    else dllTail = nullptr;
-    delete temp;
-}
-
-void DLL_DeleteTail() {
-    if (!dllTail) { cout << "Empty list!" << endl; return; }
-    NodeDLL* temp = dllTail;
-    dllTail = dllTail->prev;
-    if (dllTail) dllTail->next = nullptr;
-    else dllHead = nullptr;
-    delete temp;
-}
-
-void DLL_SearchDelete(string ip) {
-    NodeDLL* temp = dllHead;
-    while (temp && temp->ip != ip) temp = temp->next;
-    if (!temp) { cout << "There is no such node in the list!" << endl; return; }
-    if (temp == dllHead) DLL_DeleteHead();
-    else if (temp == dllTail) DLL_DeleteTail();
-    else {
-        temp->prev->next = temp->next;
-        temp->next->prev = temp->prev;
+    void deleteHead() {
+        if (head == nullptr) {
+            cout << "Error: The list is empty!" << endl;
+            return;
+        }
+        DLLNode* temp = head;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        } else {
+            tail = nullptr;
+        }
         delete temp;
     }
-}
 
-void DLL_Print() {
-    NodeDLL* temp = dllHead;
-    while (temp) {
-        cout << temp->ip << " ";
-        temp = temp->next;
+    void deleteTail() {
+        if (tail == nullptr) {
+            cout << "Error: The list is empty!" << endl;
+            return;
+        }
+        DLLNode* temp = tail;
+        tail = tail->prev;
+        if (tail != nullptr) {
+            tail->next = nullptr;
+        } else {
+            head = nullptr;
+        }
+        delete temp;
     }
-    cout << endl;
-}
+
+    void searchAndDelete(string targetIP) {
+        if (head == nullptr) {
+            cout << "Error: The list is empty!" << endl;
+            return;
+        }
+
+        DLLNode* curr = head;
+        while (curr != nullptr && curr->ip != targetIP) {
+            curr = curr->next;
+        }
+
+        if (curr != nullptr) {
+            if (curr == head) {
+                deleteHead();
+            } else if (curr == tail) {
+                deleteTail();
+            } else {
+                curr->prev->next = curr->next;
+                curr->next->prev = curr->prev;
+                delete curr;
+            }
+        } else {
+            cout << "There is no such node in the list!" << endl;
+        }
+    }
+
+    void printList() {
+        DLLNode* curr = head;
+        while (curr != nullptr) {
+            cout << curr->ip << " ";
+            curr = curr->next;
+        }
+        cout << endl;
+    }
+};
+
+
 
 int main() {
-    ifstream file("Apache_Logs.txt");
-    string line;
-    if (file.is_open()) {
-        while (getline(file, line)) {
-            if (line.empty()) continue;
-            string ip = extractIP(line);
-            SLL_InsertTail(ip);
-            DLL_InsertTail(ip);
-        }
-        file.close();
+    ifstream logFile("Apache_Logs.txt");
+    if (!logFile.is_open()) {
+        cout << "Error: Could not open Apache_Logs.txt. Ensure the file is in the same directory." << endl;
+        return 1;
     }
 
+    SLL sll;
+    DLL dll;
     int choice;
-    string ip;
+    string fileIp;
+
     while (true) {
         cout << "M E N U" << endl;
         cout << "SLL: IH(0), IT(1), DH(2), DT(3), SD(4), PS(5)" << endl;
         cout << "DLL: IH(6), IT(7), DH(8), DT(9), SD(10), PD(11)" << endl;
         cout << "Exit Program (12)" << endl;
         cout << "Choose? ";
+        
         cin >> choice;
 
-        if (choice == 12) break;
-        if (choice >= 4 && choice <= 10 && choice != 5 && choice != 6 && choice != 9) cin >> ip;
+        if (choice == 12) {
+            break;
+        }
+
+        // For insertion operations, we fetch the next IP address from the file
+        if (choice == 0 || choice == 1 || choice == 6 || choice == 7) {
+            if (logFile >> fileIp) {
+                string dummy;
+                getline(logFile, dummy); // skip the rest of the line to position for the next IP
+            } else {
+                cout << "Error: End of file reached. No more IP addresses to read." << endl;
+                continue;
+            }
+        }
+
+        string searchIp;
 
         switch (choice) {
-            case 0: cout << "Enter IP: "; cin >> ip; SLL_InsertHead(ip); break;
-            case 1: cout << "Enter IP: "; cin >> ip; SLL_InsertTail(ip); break;
-            case 2: SLL_DeleteHead(); break;
-            case 3: SLL_DeleteTail(); break;
-            case 4: SLL_SearchDelete(ip); break;
-            case 5: SLL_Print(); break;
-            case 6: cout << "Enter IP: "; cin >> ip; DLL_InsertHead(ip); break;
-            case 7: cout << "Enter IP: "; cin >> ip; DLL_InsertTail(ip); break;
-            case 8: DLL_DeleteHead(); break;
-            case 9: DLL_DeleteTail(); break;
-            case 10: DLL_SearchDelete(ip); break;
-            case 11: DLL_Print(); break;
+            // SLL Operations
+            case 0:
+                sll.insertHead(fileIp);
+                break;
+            case 1:
+                sll.insertTail(fileIp);
+                break;
+            case 2:
+                sll.deleteHead();
+                break;
+            case 3:
+                sll.deleteTail();
+                break;
+            case 4:
+                cin >> searchIp;
+                sll.searchAndDelete(searchIp);
+                break;
+            case 5:
+                sll.printList();
+                break;
+                
+            // DLL Operations
+            case 6:
+                dll.insertHead(fileIp);
+                break;
+            case 7:
+                dll.insertTail(fileIp);
+                break;
+            case 8:
+                dll.deleteHead();
+                break;
+            case 9:
+                dll.deleteTail();
+                break;
+            case 10:
+                cin >> searchIp;
+                dll.searchAndDelete(searchIp);
+                break;
+            case 11:
+                dll.printList();
+                break;
+                
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+                break;
         }
     }
+
+    logFile.close();
     return 0;
 }
